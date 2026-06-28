7a1916307aefe206137f168ebe69f915edf1fc0a
カミテ1130 <kamite11301130@gmail.com>
2026-06-29 04:16:07 +0900

    レベル　経過時間　経験値バーのUI

--- Assets/Scripts/GameScene/GameTimer.cs
+++ Assets/Scripts/GameScene/GameTimer.cs
@@ -15,6 +15,7 @@
 //-------------------------------------------------------
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameTimer : MonoBehaviour
 {
@@ -22,6 +23,16 @@ public class GameTimer : MonoBehaviour
     [SerializeField] private float targetTime = 60f;
     [Tooltip("タイマー表示に使うtextmeshの場所をアタッチ")]
     [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private Image TimerFrame;
+    [SerializeField] private Image minute10;
+    [SerializeField] private Image minute1;
+    [SerializeField] private Image second10;
+    [SerializeField] private Image second1;
+
+    [SerializeField] private Image colon;
+    [SerializeField] private Image colon2;
+
+    [SerializeField] private Sprite[] numberSprites;
     [SerializeField] private GameObject bossPrefab;
 
     private float _currentTime;
@@ -47,17 +58,25 @@ public class GameTimer : MonoBehaviour
         int minutes = (int)(_currentTime / 60f);
         int seconds = (int)(_currentTime % 60f);
 
-        if (timerText != null)
+        //if (timerText != null)
+        //{
+        //    // ボス出現中は2行目にメッセージ表示
+        //    if (_bossMessage)
+        //    {
+        //        timerText.text = $"{minutes:00}:{seconds:00}\nbossappeared";
+        //    }
+        //    else
+        //    {
+        //        timerText.text = $"{minutes:00}:{seconds:00}";
+        //    }
+        //}
+        if (numberSprites.Length >= 10)
         {
-            // ボス出現中は2行目にメッセージ表示
-            if (_bossMessage)
-            {
-                timerText.text = $"{minutes:00}:{seconds:00}\nbossappeared";
-            }
-            else
-            {
-                timerText.text = $"{minutes:00}:{seconds:00}";
-            }
+            minute10.sprite = numberSprites[minutes / 10];
+            minute1.sprite = numberSprites[minutes % 10];
+
+            second10.sprite = numberSprites[seconds / 10];
+            second1.sprite = numberSprites[seconds % 10];
         }
 
         if (_currentTime >= targetTime)
