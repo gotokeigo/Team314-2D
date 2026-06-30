4f1bd34ff0d7203a824767aadc32c7d4803ad787
DESKTOP-VFS5NOB\salmo <keigo.goto.0621@gmail.com>
2026-06-30 10:39:41 +0900

    ResultSceneの追加、プレイヤーが死んだときシーン遷移するように、esultにゲームシーンでのレベル、タイマーの情報を持っていくためにResultData.csを追加

---
+++ Assets/Scripts/ResultScene/ResultData.cs
@@ -0,0 +1,17 @@
+﻿//--------------------------------------
+//
+//  ResultData.cs
+//
+//  概要
+//  リザルト画面に渡すデータを一時的に保持するクラス
+//
+//  更新履歴
+//
+//  2026/06/30  作成
+//
+//--------------------------------------
+public static class ResultData
+{
+    public static int FinalLevel;
+    public static float SurvivedTime;
+}
---
+++ Assets/Scripts/ResultScene/ResultUI.cs
@@ -0,0 +1,30 @@
+﻿//--------------------------------------
+//
+//  ResultUI.cs
+//
+//  概要
+//  リザルト画面に最終レベルと生存時間を表示するスクリプト
+//
+//  更新履歴
+//
+//  2026/06/30  作成
+//
+//--------------------------------------
+using UnityEngine;
+using TMPro;
+using System.Security;
+
+public class ResultUI: MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    private void Start()
+    {
+        int minutes = (int)(ResultData.SurvivedTime / 60f);
+        int seconds = (int)(ResultData.SurvivedTime % 60f);
+
+        levelText.text = $"LastLevel: {ResultData.FinalLevel}";
+        timeText.text = $"SurvivalTime: {minutes:00}:{seconds:00}";
+    }
+}
